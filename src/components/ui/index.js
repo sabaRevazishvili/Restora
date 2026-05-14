e8a63081980174985bfab5a6a42dcb522869060b
Saba <revazishvilisaba37@gmail.com>
2026-05-14 23:52:58 +0400

    refactor: remove index.js files for cleaner import paths
    - Delete index.js from ui and constants folders
    - Update import statements throughout project for improved readability
    - Simplify module resolution paths

--- src/components/ui/index.js
+++
@@ -1,6 +0,0 @@
-// src/components/ui/index.js
-export { default as Button } from "./Button";
-export { default as Card } from "./Card";
-export { default as Input } from "./Input";
-export { default as ScrollReveal } from "./ScrollReveal";
-export { default as SectionDivider } from "./SectionDivider";
--- src/constants/index.js
+++
@@ -1,8 +0,0 @@
-// src/constants/index.js
-export { services } from './services';
-export { stats } from './stats';
-export { processSteps } from './process';
-export { navLinks } from './navigation';
-export { socialLinks } from './social';
-export { portfolioProjects } from './portfolio';
-export { startProjectFields } from './forms';
